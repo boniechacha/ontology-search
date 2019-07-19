2f0c7d5be3635a8dad27057afc8ef9661253386b
Boniface Chacha <bonifacechacha@gmail.com>
2019-07-19 05:17:44 +0100

    implemented a custom parser for OWL format import to fetch terms for search

--- script/import-obo.sh
+++ script/import-obo.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /home/bonifacechacha/Projects/ontology-annotation/data
-curl -v -F file=@hp.obo http://localhost:8080/api/v1/term
+curl -v -F file=@hp.obo -F namespace=HPO http://localhost:8080/api/v1/term
